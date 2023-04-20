1f5cbceba928859cf2a5af39de6219fcb6855785
matt-levy <mlevy115@gmail.com>
2023-04-20 14:03:53 -0400

    Uploading project 3

--- project/main.py
+++ project/main.py
@@ -2,11 +2,12 @@
 from flask import Flask
 from flask import *
 from flask import request 
-from movies import movies
 from logger import *
+import uuid
 
 # create an instance of a Flask object.
 app = Flask(__name__)
+movies = []
 
 @app.route("/", methods=["GET"])
 def hello_world():
@@ -15,24 +16,67 @@ def hello_world():
     '''
     return "Hello, world!"
 
-@app.route("/api/movies", methods=["GET"])
-def list_movies():
-    """
-    This function returns a Response object back containing all
-    of stored movies and reviews back to the caller on the web.
-    """
-    response_body = movies
+@app.route("/api/movies", methods=["GET", "POST"])
+def handle_movies():
+    if request.method == "GET":
+        response_body = movies
 
-    titles = request.args.getlist("title")
+        titles = request.args.getlist("title")
 
-    # only get the movies that have the same title as the query parameter.
-    if titles != []:
-        response_body = [movie for movie in movies if movie["title"] in titles]
+        # only get the movies that have the same title as the query parameter.
+        if titles != []:
+            response_body = [movie for movie in movies if movie["title"] in titles]
 
-    # use logger class to log query to log file
-    log(request.method + " " + request.url, log_file)
+        # use logger class to log query to log file
+        log(request.method + " " + request.url, log_file)
 
-    return jsonify(response_body)
+        return jsonify(response_body)
+    
+    elif request.method == "POST":
+        request_body = request.get_json()
+        
+        #checks if request body is empty or title is missing
+        if request_body == None or request_body["title"] == None:
+            return Response("Bad request", 400)
+        #checks if movie already exists in the list
+        for movie in movies:
+            if movie["title"] == request_body["title"]:
+                return Response("Already Exists", 409)
+        #creates dictionary object with movie info from request_body
+        new_movie= {"uuid": str(uuid.uuid4()), "title": request_body["title"], "review": []}
+        movies.append(new_movie)
+        return Response("Created", 201)
+        
+
+@app.route("/api/movies/<mid>", methods=["GET", "DELETE"])
+def handle_movie(mid: str) -> Response:
+    if request.method == "GET":
+        count = 0
+        if movies == []:
+            return Response("Not Found", 404)
+        for movie in movies:
+            if mid in movie.values():
+                count += 1
+        if count == 0:
+            return Response("Not Found", 404)
+        #finds movie with uuid equal to mid and returns it
+        for movie in movies:
+            if movie["uuid"] == mid:
+                return jsonify(movie)
+    elif request.method == "DELETE":
+        count = 0
+        if movies == []:
+            return Response("Not Found", 404)
+        for movie in movies:
+            if mid in movie.values():
+                count += 1
+        if count == 0:
+            return Response("Not Found", 404)
+        for movie in movies:
+            if movie["uuid"] == mid:
+                movies.remove(movie)
+                return Response("No content", 204)
+        
 
 # run our application
 if __name__ == '__main__':
